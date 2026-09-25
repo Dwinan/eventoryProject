c124f79df5ebf608d344fc08e9e3e34731366a0a
Dwinan <dann.drea08@gmail.com>
2026-09-25 14:05:47 +0700

    create migrations

---
+++ database/migrations/2026_09_24_063354_create_events_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('organization_id')->nullable()->constrained()->nullOnDelete();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('description');
+            $table->string('location');
+            $table->timestamp('start_at');
+            $table->timestamp('end_at');
+            $table->string('poster')->nullable();
+            $table->unsignedInteger('quota')->nullable();
+            $table->string('status')->default('published'); //draft, published
+            $table->timestamp('published_at')->nullable();
+            $table->foreignId('moderated_by')->nullable()->constrained('users')->nullOnDelete();
+            $table->timestamp('moderated_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('events');
+    }
+};
